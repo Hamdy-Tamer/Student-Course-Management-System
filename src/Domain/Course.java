00b1f9c0e8715a9156d185d4449f3dfefd23c29b
Hamdy-Tamer <hamdytamer253@gmail.com>
2026-06-21 13:04:48 +0300

    Implement Course class with Searchable interface

--- src/Domain/Course.java
+++ src/Domain/Course.java
@@ -1,5 +1,29 @@
 package Domain;
 import Interface.Searchable;
 
-public class Course {
+// Course Class
+public class Course implements Searchable{
+    private int courseID;
+    private String course_name;
+    private String course_code;
+
+    public Course(int courseID, String course_name, String course_code){
+        this.courseID = courseID;
+        this.course_name = course_name;
+        this.course_code = course_code;
+    }
+
+    @Override
+    public int getID() { return courseID; }
+
+    public String getCourseName() { return course_name; }
+
+    public String getCourseCode() { return course_code; }
+
+    public void display(){
+        System.out.println("");
+        System.out.println("Course ID: " + courseID);
+        System.out.println("Course Name: " + course_name);
+        System.out.println("Course Code: " + course_code);
+    }
 }
